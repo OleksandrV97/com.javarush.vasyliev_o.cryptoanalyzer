a88becc068d6d2a15898401f6de260fce4c51ddc
Oleksandr Vasyliev <vasyliev1work@gmail.com>
2024-11-11 19:08:43 +0300

    Initial commit

---
+++ src/Cipher.java
@@ -0,0 +1,17 @@
+public class Cipher {
+    private char[] alphabet;
+
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    public String encrypt(String text, int shift) {
+        // Логика шифрования
+        return text;
+    }
+
+    public String decrypt(String encryptedText, int shift) {
+        // Логика расшифровки
+        return encryptedText;
+    }
+}
---
+++ src/FileManager.java
@@ -0,0 +1,9 @@
+public class FileManager {
+    public String readFile(String filePath) {
+        // Логика чтения файла
+        return filePath;
+    }
+    public void writeFile(String content, String filePath) {
+        // Логика записи файла
+    }
+}
